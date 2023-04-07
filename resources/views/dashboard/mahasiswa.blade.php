704404ef6c144d4eebf6ea32e89d18ada44de718
bayutb123 <bayutantra28@gmail.com>
2023-04-07 07:00:47 +0700

    Edit halaman mahasiswa

--- resources/views/dashboard/mahasiswa.blade.php
+++ resources/views/dashboard/mahasiswa.blade.php
@@ -7,7 +7,6 @@
 </div>
 @stop
 @section('content')
-
 <div class="overflow-hidden bg-white shadow sm:rounded-lg">
   <div class="px-4 py-5 sm:px-6">
     <h3 class="text-base font-semibold leading-6 text-gray-900">Informasi Mahasiswa</h3>
