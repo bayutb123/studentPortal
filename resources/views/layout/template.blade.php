7031c8189aeb8dbca28408ae2095ae86c8432dc2
bayutb123 <bayutantra28@gmail.com>
2023-04-07 07:58:27 +0700

    Mengubah halaman utama dan menghubungkan halaman dashboard ke halaman utama

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -48,7 +48,7 @@
                                 <a href="/dashboard/profil" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil Mahasiswa</a>
                             </li>
                             <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                                <a href="/" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                             </li>
                         </ul>
                     </div>
